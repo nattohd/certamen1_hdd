5a097cf7d976eb571159b684dbe9436e0014a668
NattoHD <tubepika@gmail.com>
2023-09-15 14:47:59 -0300

    first commit

---
+++ src/main/java/cl/hdd/certamen1/Certamen1Application.java
@@ -0,0 +1,13 @@
+package cl.hdd.certamen1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Certamen1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Certamen1Application.class, args);
+	}
+
+}
---
+++ src/test/java/cl/hdd/certamen1/Certamen1ApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.hdd.certamen1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Certamen1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
